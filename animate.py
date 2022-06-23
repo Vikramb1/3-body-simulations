7f242a0a8ac3fa6e87a5362c8a3ad31788f388e1
vikram <bhamrev@stpaulsschool.org.uk>
2022-06-23 10:12:55 +0100

    Revert "nno"
    This reverts commit ae251fc53ecd78212602cec9b8dee8fba5619923.

--- animate.py
+++ animate.py
@@ -1,6 +1,6 @@
 import imageio
 images = []
-filenames = ['frame_proj_'+str(i)+'.png' for i in range(0,100000,100)]
+filenames = ['frame'+str(i)+'.png' for i in range(0,100000,100)]
 for filename in filenames:
     images.append(imageio.imread(filename))
-imageio.mimsave('0-100000_proj_evolution.gif', images, fps = 30)
+imageio.mimsave('0-100000_evolution.gif', images, fps = 30)
